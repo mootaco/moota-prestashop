14097df6d182a6325a7b71f47ba5ff62408c29be
Aprilus Lumbantoruan <i@pilus.me>
2017-11-29 10:50:40 +0700

    Unique Code will now be shown on Cart page

--- override/controllers/front/CartController.php
+++ override/controllers/front/CartController.php
@@ -1,42 +1,58 @@
 <?php
 
-use Moota\Prestashop\Presenter\CartPresenter;
+// This file will be copied to <PS_ROOT>/<OVERRIDE_PATH>
+// so this require path only makes sense over there
+require_once __DIR__
+    . '/../../../modules/mootapay/library/moota/moota-sdk/constants.php';
+
+use PrestaShop\PrestaShop\Adapter\Product\PriceFormatter;
 
 class CartController extends \CartControllerCore
 {
-    /**
-     * @see FrontController::initContent()
-     */
     public function initContent()
     {
-        if (
-            Configuration::isCatalogMode()
-            && Tools::getValue('action') === 'show'
-        ) {
-            Tools::redirect('index.php');
-        }
-
-        $cart = (new CartPresenter)->present(
-            $this->context->cart, $shouldSeparateGifts = true
-        );
-
-        $this->context->smarty->assign([
-            'cart' => $cart,
-            'static_token' => Tools::getToken(false),
-        ]);
-
-        if (count($cart['products']) > 0) {
-            $this->setTemplate('checkout/cart');
-        } else {
-            $this->context->smarty->assign(array(
-                'allProductsLink' => $this->context->link->getCategoryLink(
-                    Configuration::get('PS_HOME_CATEGORY')
-                ),
-            ));
-
-            $this->setTemplate('checkout/cart-empty');
-        }
-
         parent::initContent();
+
+        $smarty = $this->context->smarty;
+        $cart = $smarty->getTemplateVars('cart');
+
+        $shouldAddUqCode = count($cart['products']) > 0;
+
+        if ($shouldAddUqCode) {
+            $shouldAddUqCode = $shouldAddUqCode && isset($cart['totals']);
+        }
+
+        if ($shouldAddUqCode) {
+            $shouldAddUqCode = $shouldAddUqCode && isset(
+                $cart['totals']['total']
+            );
+        }
+
+        if ($shouldAddUqCode) {
+            $shouldAddUqCode = $shouldAddUqCode && isset(
+                $cart['totals']['total']['amount']
+            ) && $cart['totals']['total']['amount'] > 0;
+        }
+
+        $config = unserialize( \Configuration::get( MOOTA_SETTINGS ) );
+        if ( $shouldAddUqCode && $config[ MOOTA_USE_UQ_CODE ] ) {
+            $uniqueCode = mt_rand(
+                $config[ MOOTA_UQ_MIN ],
+                $config[ MOOTA_UQ_MAX ]
+            );
+
+            $cart['subtotals']['moota_uq'] = array(
+                'type' => 'payment',
+                'label' => $config['uqCodeLabel'],
+                'amount' => $uniqueCode,
+                'value' => (new PriceFormatter)->format($uniqueCode),
+            );
+        } else {
+            if (isset($cart['subtotals']['moota_uq'])) {
+                unset($cart['subtotals']['moota_uq']);
+            }
+        }
+
+        $smarty->assign(['cart' => $cart]);
     }
 }
